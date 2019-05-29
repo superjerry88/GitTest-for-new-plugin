4aa3e962daf4feb5614533cb042ba250a7f6d462
Jerry Hang <superjerry88@hotmail.com>
2019-05-29 12:36:48 +0800

    Added another line

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("this is a new line");
+            Console.WriteLine("this is going to master");
             Console.Read();
         }
     }
